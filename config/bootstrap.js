393befee2a3eb323c02df1436b8eb515ec4b0cc9
Virkapp <hodin@virk.io>
2018-04-14 11:31:22 -0500

    bootstrap

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,24 +11,13 @@
 
 module.exports.bootstrap = async function(done) {
 
-  // By convention, this is a good place to set up fake data during development.
-  //
-  // For example:
-  // ```
-  // // Set up fake development data (or if we already have some, avast)
-  // if (await User.count() > 0) {
-  //   return done();
-  // }
-  //
-  // await User.createEach([
-  //   { emailAddress: 'ry@example.com', fullName: 'Ryan Dahl', },
-  //   { emailAddress: 'rachael@example.com', fullName: 'Rachael Shaw', },
-  //   // etc.
-  // ]);
-  // ```
-
-  // Don't forget to trigger `done()` when this bootstrap function's logic is finished.
-  // (otherwise your server will never lift, since it's waiting on the bootstrap)
-  return done();
+  
+  var client = RedisService.prepareConnect().createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+  client.on('connect',function(){
+      sails.log.debug('Redis connected');
+      client.select(process.env.REDIS_DB);
+      RedisService.setConnection(client);
+    return done();
+  });
 
 };
