092c2e9bb332dd0d5553c763dc5d320fe6f9efcc
Virkapp <hodin@virk.io>
2018-04-14 13:03:17 -0500

    updates

---
+++ api/controllers/AccountController.js
@@ -0,0 +1,52 @@
+function validEmail(email) {
+  return /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(
+    email
+  );
+}
+
+module.exports = {
+	create : function(req,res){
+		var params = req.params.all();
+
+		//validamos los campos
+		if(!params.firstName || params.firstName.length <= 3)
+			return res.send(401, { error : true, message : "El nombre es obligatorio o es muy corto", status : 401 });
+		if(!params.lastName || params.lastName.length <= 3)
+			return res.send(401, { error : true, message : "El apellido es obligatorio o es muy corto", status : 401 });
+		if(!params.password || params.password.length <= 5)
+			retunr res.send(401, { error : true, message : "la contraseña es muy corta", status : 401 });
+		// validamos el email
+		if(!params.email || !validEmail(params.email))
+			return res.send(401, { error : true, message : "El correo es obligatorio ó no es valido", status : 401 });
+
+		async.waterfall([
+			function existsEmail(cb){
+				UserService.existsByEmail(params.email)
+					.then(function($exists){
+						if($exists)
+							return cb({ error : true, message : "El usuario ya existe", status : 401 });
+						return cb(null,$exists);
+					})
+					.catch(cb);
+			},
+			function createHash($exists,cb){
+				UserService.password(params.password)
+					.then(function($HASHpassword){
+						return cb(null,$HASHpassword);
+					}).catch(cb);
+			},
+			function inserUser($HASHpassword,cb){
+				UserService.create(params,$HASHpassword)
+					.then(function($user){
+						return cb(null,$user);
+					}).catch(cb);
+			}
+		], function done(err,result){
+			if(err && err.status)
+				return res.send(err.status,err);
+			else if(err)
+				return res.send(500, { error : true , message : "Internal server error", status : 500 });
+			return res.json(result);
+		});
+	}
+}
--- api/services/UserService.js
+++ api/services/UserService.js
@@ -3,6 +3,42 @@ var bcrypt = require('bcrypt');
 const saltRounds = 10
 
 module.exports = {
+	password: function(password) {
+	return new Promise(function(resolve, reject) {
+	  bcrypt.genSalt(saltRounds, function(err, salt) {
+	    if (err)
+	      return reject({
+	        error: true,
+	        message: 'Internal server error',
+	        status: 500
+	      })
+	    bcrypt.hash(password, salt, function(err, hash) {
+	      if (err)
+	        return reject({
+	          error: true,
+	          message: 'Internal server error',
+	          status: 500
+	        })
+	      return resolve(hash)
+	    })
+	  })
+	})
+	},
+	existsByEmail : function(email){
+		return new Promise(function(resolve, reject) {
+			User.native(function(err, collection) {
+				if(err)
+					return reject({ error : true, message : "Internal server error" , status : 500 });
+				collection.count({
+					email : email
+				},function(err,total){
+					if(err)
+						return reject({ error : true, message : "Internal server error", statis : 500 });
+					return resolve( (total ? true : false  ) );
+				});
+			})
+		});
+	},
 	create : function(params,password){
 		//recuerda usar siempre promise se vera mas pro tu desarrollo ;)
 		return new Promise(function(resolve, reject) {
