4dcab66ef95775ce0dd858c7454966fd657634af
Virkapp <hodin@virk.io>
2018-04-14 22:57:54 -0500

    login validation

--- api/controllers/AccountController.js
+++ api/controllers/AccountController.js
@@ -5,6 +5,24 @@ function validEmail(email) {
 }
 
 module.exports = {
+	login  : function(req,res){
+		var params = req.allParams();
+		if(!params.email || !validEmail(params.email))
+			return res.send(401, { error : true, message : "El correo es obligatorio ó no es valido", status : 401 });
+		if(!params.password || params.password.length <= 5)
+			return res.send(401, { error : true, message : "la contraseña es muy corta", status : 401 });
+		async.waterfall([
+			function existUser(cb){
+				UserService.existsByEmail(params.email)
+			}
+		],function done(err,result){
+			if(err && err.status)
+				return res.send(err.status,err);
+			else if(err)
+				return res.send(500, { error : true , message : "Internal server error", status : 500 });
+			return res.json(result);
+		}); 
+	},
 	create : function(req,res){
 		var params = req.allParams();
 
